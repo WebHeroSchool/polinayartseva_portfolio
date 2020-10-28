9f7526931bb9a45cf642b8adf7ad545fea0e7fb5
PolinaYartseva <polina_yartseva@hotmail.com>
2020-10-28 17:54:47 +0200

    3rd commit3rd commit3rd commit

--- script.js
+++ script.js
@@ -3,23 +3,29 @@ window.onscroll = function() {
 };
 
 const nav = document.getElementById("nav");
-const menuLink = document.getElementsByClassName("menu__link");
+const menuLink = document.querySelectorAll(".menu__link");
 
 const sticky = nav.offsetTop;
 
 function checkMarginToTop() {
     if (window.pageYOffset > sticky) {
         nav.classList.add("sticky");
-        //menuLink.classList.add("menu__link-white");
+        menuLink.forEach((el) => {
+            el.classList.add("menu__link-white");
+        })
     } else {
         nav.classList.remove("sticky");
-        //menuLink.classList.remove("menu__link-white");
+        menuLink.forEach((el) => {
+            el.classList.remove("menu__link-white");
+        })
     }
 }
 
-const header = document.getElementById("header");
+//const header = document.getElementById("header");
 
-window.load = function(){
+let screenWidth;
+
+window.onLoad = function(){
     // Теперь мы знаем актуальную ширину экрана
     let screenWidth = parseInt(window.getComputedStyle(document.screen, null)
         .getPropertyValue('width'), 10);
@@ -35,9 +41,9 @@ window.load = function(){
 };
 
 function menuSwitch() {
-    if (screenWidth <= 480) {
-        header.classList.toggle("burger__menu");
+    if (screenWidth <= 768) {
+        nav.classList.toggle("burger__menu");
     } else {
-        header.classList.toggle("header__menu");
+        nav.classList.toggle("header__menu");
     }
 }
